45cd810fe2e7ad441189326ee89abc386e82bfd7
jiayuxin <13520723393@163.com>
2020-09-08 11:28:07 +0800

    frequent to frequency

--- src/bin/list.rs
+++ src/bin/list.rs
@@ -6,17 +6,17 @@ use std::iter::FromIterator;
 #[derive(Clone, Eq, PartialEq,Debug)]
 pub struct Node {
     inner:char,
-    frequent:u32,
+    frequency:u32,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
 }
 
 impl  Node {
     #[inline]
-  pub fn new(inner:char,frequent:u32)->Self{
+  pub fn new(inner:char,frequency:u32)->Self{
         Node{
             inner,
-            frequent,
+            frequency,
             left: None,
             right: None,
         }
@@ -25,7 +25,7 @@ impl  Node {
 
 impl  Ord for Node {
     fn cmp(&self,other:&Node)->Ordering{
-        other.frequent.cmp(&self.frequent)
+        other.frequency.cmp(&self.frequency)
     }
 }
 
@@ -56,7 +56,7 @@ pub fn build_trie(forest:Vec<Node>)->Option<Node> {
         match first {
             Some(first)=> match second {
                 Some(second)=>{
-                    let mut parent =Node::new('\0',first.frequent+second.frequent);
+                    let mut parent =Node::new('\0',first.frequency+second.frequency);
                     // 在这里将 first move 到 Box 里面并设置到父节点的left
                     parent.left=Some(Box::from(first));
                     // 取消下面的注释会报错: value borrowed here after move
