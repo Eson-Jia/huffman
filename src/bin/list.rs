5e6e29906ff245f11b728c9ad91a2267727da3e8
jiayuxin <13520723393@163.com>
2020-09-08 11:09:03 +0800

    重构代码使用函数来封装逻辑

--- src/bin/list.rs
+++ src/bin/list.rs
@@ -1,13 +1,14 @@
 use std::cmp::Ordering;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, LinkedList};
+use std::iter::FromIterator;
 
 
 #[derive(Clone, Eq, PartialEq,Debug)]
 pub struct Node {
-    pub inner:char,
-    pub frequent:u32,
-    pub left: Option<Box<Node>>,
-    pub right: Option<Box<Node>>,
+    inner:char,
+    frequent:u32,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
 }
 
 impl  Node {
@@ -22,7 +23,6 @@ impl  Node {
     }
 }
 
-
 impl  Ord for Node {
     fn cmp(&self,other:&Node)->Ordering{
         other.frequent.cmp(&self.frequent)
@@ -35,16 +35,19 @@ impl  Ord for Node {
      }
  }
 
-fn main(){
-    // let mut head = Node::new('d',123);
-    // head.left = Some(Box::new(Node::new('b',21)));
-    let mut the_heap = BinaryHeap::new();
+pub fn build_forest(text:&str)-> Vec<Node>{
+    let mut the_forest = Vec::new();
     for i in 0..26 {
         match std::char::from_u32(97+i) {
-            Some(char)=>the_heap.push(Node::new(char,i+1)),
+            Some(char)=> the_forest.push(Node::new(char, i+1)),
             None=> panic!("isn't a ascii"),
         }
     }
+    the_forest
+}
+
+pub fn build_trie(forest:Vec<Node>)->Option<Node> {
+    let mut the_heap  =  BinaryHeap::from(forest);
     while the_heap.len()>1{
         let  first = the_heap.pop();
         let  second = the_heap.pop();
@@ -64,16 +67,8 @@ fn main(){
             None=>panic!("first is None"),
         }
     };
-    if let Some(root) =  the_heap.pop() {
-        // println!("{:?}",root);
-       let result= get_last(&root);
-        println!("{:?}",result);
-        let result= get_last(&root);
-        println!("{:?}",result);
-    };
+    the_heap.pop()
 }
-
-
 
 pub fn get_last(root: &Node)->&Node {
     let mut lhs = root;
@@ -81,5 +76,17 @@ pub fn get_last(root: &Node)->&Node {
         lhs = node.as_ref();
     }
     lhs
+}
+
+fn main(){
+    let text = "this is the best of time,this is the worst of time";
+    let  root  =  build_trie(build_forest(text));
+    if let Some(root) = root{
+        // println!("{:?}",root);
+        let result= get_last(&root);
+        println!("{:?}",result);
+        let result= get_last(&root);
+        println!("{:?}",result);
+    };
 }
 
